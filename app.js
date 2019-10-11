880a0631e44f4423d61082e1cdd9332612100dad
albert18 <caps18@hotmail.com>
2019-10-10 23:57:55 -0400

    update

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const express = require('express');
 
 const app = express();
 
+// middleware
+app.use((req, res, next) => {
+    console.log('In the middle ware')
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('In another the middle ware')
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);  
