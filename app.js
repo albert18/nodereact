0b1b1faeabc568eea0e5016816e33e5eaa3a22ac
albert18 <caps18@hotmail.com>
2019-10-14 22:13:48 -0400

    update

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-// middleware
-app.use((req, res, next) => {
-    console.log('In the middle ware')
-    // to reach next middleware
-    next(); 
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use((req, res, next) => {
-    console.log('In another the middle ware TEST')
-    res.send('<h1>Hello</h1>')
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
 
 
-const server = http.createServer(app);
-
-server.listen(3000);  
+// app.listen(3000); //Alternative
+// // const server = http.createServer(app);
+// // server.listen(3000);  
