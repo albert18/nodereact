93130226d43cd0d39e126d36952656c17484134c
albert18 <caps18@hotmail.com>
2019-10-16 07:58:03 -0400

    update

--- app.js
+++ app.js
@@ -3,20 +3,15 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const shopRoutes = require('./routes/shop');
+const adminRoutes = require('./routes/admin');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
+// using routes
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
-});
 
 app.listen(3000);
 
