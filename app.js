463cb8dcfda2127d9b44463bca9b32517941ad41
albert18 <caps18@hotmail.com>
2019-10-17 20:56:40 -0400

    update section

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -14,7 +16,7 @@ app.use(shopRoutes);
 
 // Handling Error
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not Found</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
